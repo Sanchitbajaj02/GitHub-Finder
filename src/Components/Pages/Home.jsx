ea8ff0e6583408f0ee3aae981b55060cc5a8e834
Sanchit <sanchit02@outlook.com>
2021-12-22 13:56:23 +0530

    added minor cards for navigation

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <React.Fragment>
+      <div className="row">
+        <div className="col-md-6">
+          <div className="card card-shadow">
+            <Link
+              to="/users"
+              className="card-body link-card-highlight text-center">
+              <h4 className="card-title">User Search</h4>
+            </Link>
+          </div>
+        </div>
+        <div className="col-md-6">
+          <div className="card card-shadow">
+            <Link
+              to="/orgs"
+              className="card-body link-card-highlight text-center">
+              <h4 className="card-title">Organization Search</h4>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default Home;
